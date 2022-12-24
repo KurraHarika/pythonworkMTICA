23c3d6d54fa2cecad1157ba8e404bddc9facb2ac
KurraHarika <121171553+KurraHarika@users.noreply.github.com>
2022-12-24 16:58:30 +0530

    Add files via upload

---
+++ program8.py
@@ -0,0 +1,17 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a='today is saturday'
+a.replace('at','****')
+'today is s****urday'
+a
+'today is saturday'
+a=a.replace('at','****')
+a
+'today is s****urday'
+a=a.replace('****','at')
+a
+'today is saturday'
+a='computer'
+a=a.replace('put','python')
+a
+'compythoner'
